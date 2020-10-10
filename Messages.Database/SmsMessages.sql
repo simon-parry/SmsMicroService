947cedd2864915f2425494bf3155e5eaa7c736fa
simon-parry <72634671+simon-parry@users.noreply.github.com>
2020-10-10 01:25:59 +0100

    Add files via upload

---
+++ Messages.Database/SmsMessages.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[SmsMessages]
+(
+	[SmsMessagesId] [int] IDENTITY(1,1) NOT NULL,
+	[PhoneNumber] [varchar](15) NULL,
+	[MessageContent] [varchar](2000) NULL,
+	[Result] [int] NULL,
+	[DateAdded] [datetime] NULL,
+	[DateSent] [datetime] NULL,
+	[ResponseDate] [datetime] NULL, 
+    CONSTRAINT [PK_Messages] PRIMARY KEY ([SmsMessagesId])
+)
+
+GO
+
+ALTER TABLE [dbo].[SmsMessages] ADD  CONSTRAINT [DF_Message_Result]  DEFAULT ((0)) FOR [Result]
+GO
+
+ALTER TABLE [dbo].[SmsMessages] ADD  CONSTRAINT [DF_Message_DateAdded]  DEFAULT (getdate()) FOR [DateAdded]
+GO
