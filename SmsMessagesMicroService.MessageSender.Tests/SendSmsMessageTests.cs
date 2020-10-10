29d04f777defe67f9856634b3e76d6362a44ea81
simon-parry <72634671+simon-parry@users.noreply.github.com>
2020-10-10 01:36:19 +0100

    Add files via upload

---
+++ SmsMessagesMicroService.MessageSender.Tests/SendSmsMessageTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Options;
+using Moq;
+using SmsMessagesMicroService.MessageSender.QueueEntities;
+using Xunit;
+
+namespace SmsMessagesMicroService.MessageSender.Tests
+{
+    public class SendSmsMessageTests
+    {
+        private readonly Mock<IOptions<RabbitMqConnection>> _rabbitMqOptions;
+        private readonly SendSmsMessage _instance;
+
+        public SendSmsMessageTests()
+        {
+            _rabbitMqOptions = new Mock<IOptions<RabbitMqConnection>>();
+
+            _rabbitMqOptions.Setup(x => x.Value)
+                .Returns(new RabbitMqConnection()
+                {
+                    Hostname = "localhost",
+                    Port = 15267,
+                    UserName = "admin",
+                    Password = "admin",
+                    QueueName = "MessagesQueue"
+                });
+
+            _instance = new SendSmsMessage(_rabbitMqOptions.Object);
+        }
+
+        [Fact]
+        public void TestSuccessfullyAddingMessageToQueue()
+        {
+
+            var result = _instance.Send(It.IsAny<Sms>());
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void TestUnSuccessfullyAddingMessageToQueue()
+        {
+            var result = _instance.Send(It.IsAny<Sms>());
+            Assert.False(result);
+        }
+    }
+}
